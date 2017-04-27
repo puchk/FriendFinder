81f1db0d8ebe00b179bff6040a0f3b50c328a8c3
puchk <kpuchalski@gmail.com>
2017-04-27 02:23:37 -0700

    Created an array of objects to hold info for each friend

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendsArray = [
+	{ "name":"Michael",
+		"photo":"http://www.thesmokinggun.com/sites/default/files/imagecache/670xX/photos/mvick20082mug1.jpg",
+		"scores":[
+			1,
+			5,
+			4,
+			2,
+			3,
+			1,
+			5,
+			2,
+			5,
+			5]
+	}
+];
+
+module.exports = friendsArray;
