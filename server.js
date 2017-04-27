fe16e4bdd47ae57fb21f7cfc68e2420c863f9625
puchk <kpuchalski@gmail.com>
2017-04-27 02:10:04 -0700

    Updated html pages and serverjs

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Express Instance
+var app = express();
+
+// Sets an initial port to use later in our listener
+var PORT = process.env.PORT || 8080;
+
+// BodyParser for our server to interpret data sent to it.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// // Router
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
